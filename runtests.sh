10a678801b77d26003880d0bcdafecfbd1c80c7f
Muhamad Salman <muhamad.salman2001@gmail.com>
2026-06-28 17:32:46 +0200

    feat: added real, string, left-tape, and enum expansion tests

--- runtests.sh
+++ runtests.sh
@@ -30,7 +30,7 @@ run_test() {
     
     echo -n "[$TOTAL] $name ... "
     
-    local output exitcode
+    local output exitcode=0
     output=$(get_output "$subcmd" "$file") || exitcode=$?
     
     if [ "$MODE" = "record" ]; then
@@ -60,5 +60,9 @@ run_test "01-inc"         "run" "examples/01-inc.tape"
 run_test "02-pairs"       "run" "examples/02-pairs.tape"
 run_test "03-magical"     "run" "examples/03-magical.tape"
 run_test "04-eval"        "run" "examples/04-eval.tape"
+run_test "real"           "run" "tests/real.tape"
+run_test "string"         "run" "tests/string.tape"
+run_test "left-tape"      "run" "tests/left-tape.tape"
 run_test "unused-vars"    "run" "tests/unused-vars.tape"
 run_test "01-inc-expand"  "expand" "examples/01-inc.tape"
+run_test "enum-expand"    "expand --enum" "examples/01-inc.tape"
